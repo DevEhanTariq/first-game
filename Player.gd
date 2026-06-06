00bb1294eb3ba7f320363de4e5ce819df4de0b2e
DevET <ehan.tariq.21@icloud.com>
2026-06-06 19:18:06 +0100

    New Player Movement System
    - Changed the current gradual movement for a fixed 3 position only movment
    - The player can switch between only 3 lanes

--- Player.gd
+++ Player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-@export var speed = 40000
+@export var lane = 0
+@export var laneChangeRate = 0.25
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,5 +11,18 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var input_direction = Input.get_vector("none", "none", "up", "down")
-	velocity = input_direction * speed * delta
-	move_and_slide()
+	if input_direction[1] != 0:
+		lane += input_direction[1]/laneChangeRate * delta
+		var nLane = round(lane)
+	
+		if nLane > 1:
+			lane = 1
+		elif nLane < -1:
+			lane = -1
+	
+		if nLane == -1:
+			position.y = -300
+		elif nLane == 0:
+			position.y = 0
+		elif nLane == 1:
+			position.y = 300
