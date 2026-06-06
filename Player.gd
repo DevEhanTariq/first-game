56974da6f2b59664946517c2b91a16bc1d43d528
DevET <ehan.tariq.21@icloud.com>
2026-06-06 18:30:57 +0100

    Added FPS Counter and Collisions to the Player
    - Add "Label" node in the "Level" scene
    - Added script to "Label" node, and created an FPS Counter
    - Added an "Area2D" and a "CollisionShape2D" node to the "Sprite2D" node in the "Player" scene

--- Player.gd
+++ Player.gd
@@ -1,5 +1,6 @@
 extends Sprite2D
 
+@export var speed = 400
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +9,5 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var input_direction = Input.get_vector("none", "none", "up", "down")
+	var velocity = input_direction * speed * delta
