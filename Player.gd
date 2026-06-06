264ddc9dc82336c0f5c6ae80489542c499debb19
DevET <ehan.tariq.21@icloud.com>
2026-06-06 18:50:56 +0100

    Added Movment & re-structured the "Player" scene
    - Deleted "Area2D"
    - Put eveything in the "Player" scene, under "CharacterBody2D"
    - Added Player Movement (is really choppy)

--- Player.gd
+++ Player.gd
@@ -1,6 +1,6 @@
-extends Sprite2D
+extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 40000
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,4 +10,5 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var input_direction = Input.get_vector("none", "none", "up", "down")
-	var velocity = input_direction * speed * delta
+	velocity = input_direction * speed * delta
+	move_and_slide()
