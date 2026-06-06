ed1cd72baf7da0d57d777c230ed1a96986947873
DevET <ehan.tariq.21@icloud.com>
2026-06-06 20:32:41 +0100

    Old Roads now deleted
    - Now puts all roads in a list, and when there are 5 roads, it deletes the oldest one

--- roadGen.gd
+++ roadGen.gd
@@ -3,6 +3,7 @@ extends Node2D
 var allowRoad = true
 var defaultPos = 2000
 var roadLength = 2000
+var roads = []
 var road = 0
 
 var new_road: Sprite2D
@@ -18,17 +19,18 @@ func spawn_road():
 		new_road = $Road.duplicate()
 		add_child(new_road)
 		new_road.position = Vector2((defaultPos + (roadLength*road))-500, 0)
-		delete_after_delay(new_road, 5.0)
+		roads.append(new_road)
 		
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
+func delete_oldest_road():
+	if roads.size() > 4:
+		var oldest_road = roads.pop_front()
+		oldest_road.queue_free()
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_float):
 	spawn_road()
 	var latestRoadPos = new_road.position[0]
+	
 	if latestRoadPos <= (roadLength):
 		allowRoad = true
 		road = 0
@@ -36,3 +38,5 @@ func _process(_float):
 		allowRoad = true
 		road = 1
 		spawn_road()
+		
+		delete_oldest_road()
