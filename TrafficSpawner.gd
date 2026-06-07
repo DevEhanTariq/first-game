2b8805dd12413d7408a0d7a7d9badaecf4d52f6e
DevET <ehan.tariq.21@icloud.com>
2026-06-07 16:35:37 +0100

    Spawning Traffic
    - Added functioning random spawning of traffic in different colors

--- TrafficSpawner.gd
+++ TrafficSpawner.gd
@@ -2,21 +2,37 @@ extends Node
 
 var x = 2000
 var lanesY = [-300, 0, 300]
+var lane = [[0,1],[1,2],[0,2]]
 
 var allowCar = true
 var cars = []
-var carsAmountPerY = []
+var speed = 300
 
-func spawn_car(lane, xCord):
-	var sibling = get_parent().get_node("Traffic")
-	var copy = sibling.duplicate()
-	copy.position = Vector2(xCord,lanesY[lane])
-	cars.append(copy)
-	get_parent().add_child(copy)
-		
-func delete_oldest_road():
-	pass
-		
+func spawn_car(xCord):
+	var lanesToUse = lane[randi_range(0,2)]
+	for i in range(2):
+		if allowCar:
+			var sibling = get_parent().get_node("Traffic")
+			var copy = sibling.duplicate()
+			cars.append(copy)
+			copy.position = Vector2(xCord,lanesY[lanesToUse[i]])
+			get_parent().add_child(copy)
+	allowCar = false
+
+func delete_oldest_cars():
+	if cars[0].position.x < -1500:
+		var oldest_cars = cars.pop_front()
+		oldest_cars.queue_free()
+		oldest_cars = cars.pop_front()
+		oldest_cars.queue_free()
+
+func CarPatternSpawner():
+	if cars[-1].position.x < 800:
+		allowCar = true
+
+func moveCars(delta: float):
+	for i in cars:
+		i.position.x -= speed * delta 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -24,5 +40,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	spawn_car(randi_range(0,2), 1500)
+	spawn_car(2000)
+	CarPatternSpawner()
+	delete_oldest_cars()
+	moveCars(delta)
+	print(cars.size())
 	
